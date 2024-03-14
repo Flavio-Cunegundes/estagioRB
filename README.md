b3a94302cbc5dc20049287e4949ae47890c2766f
Flávio Cunegundes Pinho <120675038+Flavio-Cunegundes@users.noreply.github.com>
2024-03-14 18:00:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# estagioRB
+ repositório referente a uma das etapas de avaliação de um processo seletivo realizado por Flávio Cunegundes Piho
---
+++ soma.js
@@ -0,0 +1,5 @@
+const cursoTodos=[...document.getElementsByClassName("curso")]
+const curso1=[...document.getElementsByClassName("1")]
+const curso2=[...document.getElementsByClassName("2")]
+
+console.log(curso1)
