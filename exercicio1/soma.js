885e4dd0e43bcbd8705ef777d3dcff4d03e1e71c
Flávio Cunegundes Pinho <120675038+Flavio-Cunegundes@users.noreply.github.com>
2024-03-14 19:59:51 -0300

    up
    fcp

--- exercicio1/soma.js
+++
@@ -1,5 +0,0 @@
-const cursoTodos=[...document.getElementsByClassName("curso")]
-const curso1=[...document.getElementsByClassName("1")]
-const curso2=[...document.getElementsByClassName("2")]
-
-console.log(curso1)
